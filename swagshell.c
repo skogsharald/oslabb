1b87f2abf7f0a9d7eeee00b4a3bca76e477fa620
Ludvig Jansson <ludjan@kth.se>
2015-04-16 14:54:51 +0200

    swag

--- swagshell.c
+++ swagshell.c
@@ -3,11 +3,13 @@
 #define MAX_LENGTH 80
 int main() {
 	char cmd [MAX_LENGTH];
+
 	while(1) {
 		printf("swag > ");
 
 		if(fgets(cmd, sizeof(cmd), stdin) == NULL) break;
 		printf("%s", &cmd[0]);
+
 	}
 	return EXIT_SUCCESS;
 }
