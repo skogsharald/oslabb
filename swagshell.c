3233722b653485b62c9a706deceba7f49b4ae1e5
Ludvig Jansson <ludjan@kth.se>
2015-04-16 14:46:29 +0200

    swag

--- swagshell.c
+++ swagshell.c
@@ -1,10 +1,16 @@
 #include "swagshell.h"
 
-int main(int argc, char *argv[]) {
-	int status = changeDir(argv[1]);
-	printf("%d", status);
-	printf("%s", argv[2]);
-	return status;
+#define MAX_LENGTH 80
+int main() {
+	char cmd [MAX_LENGTH];
+	while(1) {
+		printf("swag > ");
+
+		scanf("%[^\n]%*c", cmd);
+		if(cmd[0] == NULL) break;
+		printf("%s", &cmd[0]);
+	}
+	return EXIT_SUCCESS;
 }
 
 int changeDir(const char *dir) {
