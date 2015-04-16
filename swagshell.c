686b8ce851abbbfe7d5e852eb5662d41ef72b808
Ludvig Jansson <ludjan@kth.se>
2015-04-16 14:29:16 +0200

    swag

--- swagshell.c
+++ swagshell.c
@@ -1,6 +1,12 @@
 #include "swagshell.h"
 
 int main(int argc, char *argv[]) {
-	printf("%d", argc);
-	return EXIT_SUCCESS;
+	int status = changeDir(argv[1]);
+	printf("%d", status);
+	printf("%s", argv[2]);
+	return status;
+}
+
+int changeDir(const char *dir) {
+	return chdir(dir);
 }
--- swagshell.h
+++ swagshell.h
@@ -1,2 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+/* Declaration of the changeDir function */
+int changeDir(const char*);
